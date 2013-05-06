6ffa6d9e1758a63b83581d8b19949ffa11e3d183
Robert Poor <rdpoor@gmail.com>
2013-05-06 08:02:29 -0700

    adding explicit .json to API call to POST

--- lib/cosm_writer.py
+++ lib/cosm_writer.py
@@ -9,14 +9,14 @@ class CosmWriter(Subject, Observer):
     def __init__(self):
         Subject.__init__(self)
         Observer.__init__(self)
-
+        
     API_PREFIX='http://api.cosm.com/v2'
     FEED_ID='129722'
 
     # support for observer
 
     def update(self, subject, message):
-        r = requests.post(self.API_PREFIX + '/feeds/' + self.FEED_ID + '/datastreams/1/datapoints',
+        r = requests.post(self.API_PREFIX + '/feeds/' + self.FEED_ID + '/datastreams/1/datapoints.json',
                           data=json.dumps(message),
                           headers = {'X-ApiKey': self.cosm_api_key()})
         if r.status_code >= 300:
